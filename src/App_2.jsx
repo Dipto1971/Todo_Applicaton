783b31379d518ab88c2d96157e232a84f9ba1894
Mahir Faysal Dipto <dipto.haq771@gmail.com>
2024-06-14 12:30:44 +0600

    Naive Solution of renderring only once

--- src/App_2.jsx
+++ src/App_2.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 
+var ctr =`0`;
 function App_2() {
   const [todos, setTodos] = React.useState(
       {
@@ -11,13 +12,24 @@ function App_2() {
   );
 
   console.log("rendering");
-  // This will cause too many re renders because react will
-  // keep on calling the useEffect as it is how the useEffect works
-    setTodos({
-        id: 1,
-        title: "Go to Gym at: " + Math.random()* 1000,
-        completed: true
-      })
+  // In react, rendering happens again and again
+  // That's why setTimeout is occuring again and again
+  
+  if(ctr === `0`){
+    setTimeout(() => {
+      setTodos({
+          id: 1,
+          title: "Go to Gym at: " + Math.random()* 1000,
+          completed: true
+        })
+    }, 1000);
+
+    ctr = 1;
+  }
+
+  // This is a naive solution to avoid multiple rendering
+  // By this way, we can avoid multiple rendering
+  // Only when ctr === 0, it will render. Else it will not render
 
   return (
     <div>
